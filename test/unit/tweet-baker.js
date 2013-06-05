dbb3932887f9168e7bf40a04b5634d499761b2c4
Rowan Manning <accounts@rowanmanning.co.uk>
2013-06-05 20:08:58 +0100

    Flesh out the basic guarding tests

--- test/unit/tweet-baker.js
+++ test/unit/tweet-baker.js
@@ -1,27 +1,59 @@
 /* global describe, it */
 'use strict';
 
+var assert = require('proclaim');
+var tb = require('./../../lib/tweet-baker');
+
 describe('baker', function () {
 
     describe('.createEntityBaker()', function () {
 
-        it('should not throw when called with no arguments');
+        it('should not throw when called with no arguments', function () {
+            assert.doesNotThrow(function () {
+                tb.createEntityBaker();
+            });
+        });
 
-        it('should not throw when called with an object first argument');
+        it('should not throw when called with an object first argument', function () {
+            assert.doesNotThrow(function () {
+                tb.createEntityBaker({});
+            });
+        });
 
-        it('should throw when called with an unempty, non-object first argument');
+        it('should throw when called with an unempty, non-object first argument', function () {
+            assert.throws(function () {
+                tb.createEntityBaker('foo');
+            }, /object expected/i);
+            assert.throws(function () {
+                tb.createEntityBaker([]);
+            }, /object expected/i);
+        });
 
-        it('should return a function (bake)');
+        it('should return a function (bake)', function () {
+            assert.isFunction(tb.createEntityBaker());
+        });
 
     });
 
     describe('.bakeEntities()', function () {
 
-        it('should not throw when called with valid arguments');
+        it('should not throw when called with valid arguments', function () {
+            assert.doesNotThrow(function () {
+                tb.bakeEntities('foo', {});
+            });
+        });
 
-        it('should throw when called with a non-string first argument');
+        it('should throw when called with a non-string first argument', function () {
+            assert.throws(function () {
+                tb.bakeEntities(null, {});
+            });
+        });
 
-        it('should throw when called with a non-object second argument');
+        it('should throw when called with a non-object second argument', function () {
+            assert.throws(function () {
+                tb.bakeEntities('foo', null);
+            });
+        });
 
         it('should return the expected HTML');
 
